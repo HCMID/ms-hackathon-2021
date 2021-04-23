0d060e5e2fec5304abce377a84f00ccdd04b4b15
Neel Smith <dnsmith.neel@gmail.com>
2021-04-23 16:30:46 -0400

    Start scripting out image browsing page

---
+++ scripts/README.md
@@ -0,0 +1,18 @@
+Object IDs in URNs look like
+
+urn:cite2:citeecod:codsang250imgs.v1:sg250_218_217_0
+
+where the 218 is the *preceding* page and the 217 is the *current* page
+
+It would be nicer to base these on ecodices DOIs.
+
+But that's not happening for the '21 hackathon.
+
+so script for n in 2:650 and form 
+
+(n-1)_n 
+
+in template name string.
+
+Use CitableImage to build a nice page.
+
--- scripts/thumbs.jl
+++ scripts/thumbs.jl
@@ -7,4 +7,15 @@ root = "/project/homer/pyramidal/deepzoom"
 service = IIIFservice(baseurl, root)
 
 ict = "http://www.homermultitext.org/ict2/?"
-linkedMarkdownImage(ict, img, service; ht=100, caption="folio 12 recto")
+
+
+urnbase = "urn:cite2:citeecod:codsang250imgs.v1:sg250"
+
+urns = []
+for n in 2:650 
+    str = string(urnbase, "_", n, "_", (n - 1), "_0")
+    push!(urns,Cite2Urn(str))
+end
+
+
+#linkedMarkdownImage(ict, img, service; ht=100, caption="folio 12 recto")
